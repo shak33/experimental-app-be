371febb023cca2a41bbcad7b9169b981742b3daf
shak33 <maciej@poppek.pl>
2025-09-25 11:42:27 +0200

    Configured initial node server

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+require('dotenv').config();
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+export const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.options('*', cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ success: true, message: 'API is running' });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'API endpoint does not exist',
+  });
+});
