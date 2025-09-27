8d1377593ea56cacb7a686bc2c6d158ecb45e43e
shak33 <maciej@poppek.pl>
2025-09-27 14:18:15 +0200

    Extended global Express Request model to include user related data such as id, userId and email, to use them in REST API requests, created middleware to check if comming request comes from authenticated user

---
+++ src/middlewares/isAuthenticated.middleware.ts
@@ -0,0 +1,85 @@
+import jwt, { JsonWebTokenError, JwtPayload, TokenExpiredError } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { prisma } from '@/db';
+
+export const isAuthenticatedMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader) {
+      return res.status(401).json({
+        success: false,
+        message: 'Unauthorized',
+      });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: 'Unauthorized',
+      });
+    }
+
+    let verify: JwtPayload | string;
+
+    try {
+      verify = jwt.verify(token, process.env.JWT_SECRET as string);
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        return res.status(401).json({
+          success: false,
+          message: 'Token has expired',
+        });
+      } else if (error instanceof JsonWebTokenError) {
+        return res.status(401).json({
+          success: false,
+          message: 'Unauthorized',
+        });
+      }
+
+      return res.status(500).json({
+        success: false,
+        message: 'Internal server error',
+      });
+    }
+
+    if (typeof verify !== 'object' || !('id' in verify)) {
+      return res.status(401).json({
+        success: false,
+        message: 'Unauthorized',
+      });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: verify.id,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: 'Unauthorized',
+      });
+    }
+
+    req.user = {
+      email: user.email,
+      id: user.id,
+      userId: user.userId,
+    };
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+    });
+  }
+};
